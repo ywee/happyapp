264621718da6ea926ab5232aa15989332ea053a4
Yeong Wee <ywee@sas.upenn.edu>
2013-05-10 14:59:11 -0400

    finalfinal

--- spec/features/event_spec.rb
+++ spec/features/event_spec.rb
@@ -1,9 +1,8 @@
 require 'spec_helper'
 
 describe 'Adding an Event' do
-	it 'requires a name' do
+	it 'requires a name', :js => true do
 		visit '/event_reqs/new'
-		click_link "Create New Event"
 		click_button "Create Event"
 		error_message = "Friend can't be blank"
 		page.should have_content(error_message)
